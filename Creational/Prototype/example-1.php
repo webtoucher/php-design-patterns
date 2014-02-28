657831b810f164377a07d54ab1dc72513c234b09
Алексей Кузнецов <kuznetsov.aleksey@caesber.ru>
2014-02-28 13:43:36 +0700

    Added Prototype example

---
+++ Creational/Prototype/example-1.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Some product
+ */
+interface Product
+{
+}
+
+/**
+ * Prototype Factory
+ */
+class PrototypeFactory
+{
+
+    /**
+     * @var Product
+     */
+    private $product;
+
+
+    /**
+     * @param Product $product
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Returns new instance of the product by cloning
+     *
+     * @return Product
+     */
+    public function getProduct()
+    {
+        return clone $this->product;
+    }
+}
+
+/**
+ * The first product
+ */
+class SomeProduct implements Product
+{
+    public $name;
+}
+
+/*
+ * =====================================
+ *          USING OF PROTOTYPE
+ * =====================================
+ */
+
+$prototypeFactory = new PrototypeFactory(new FirstProduct());
+$firstProduct = $prototypeFactory->getProduct();
+$firstProduct->name = 'The first product';
+$secondProduct = $prototypeFactory->getProduct();
+$secondProduct->name = 'Second product';
+
+print_r($firstProduct->name);
+// The first product
+print_r($secondProduct->name);
+// Second product
