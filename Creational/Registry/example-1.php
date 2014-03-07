71e4a8fd1bf8e09c1bffed933f551147cb3368f4
Алексей Кузнецов <kuznetsov.aleksey@caecber.ru>
2014-03-08 00:45:12 +0700

    Added Registry example

---
+++ Creational/Registry/example-1.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Registry
+ *
+ * @package Patterns
+ */
+class Product
+{
+
+    /**
+     * @var Product[]
+     */
+    protected static $data = array();
+
+
+    /**
+     * Adds variable into the registry
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public static function set($key, $value)
+    {
+        self::$data[$key] = $value;
+    }
+
+    /**
+     * Returns value from the registry by key
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public static function get($key)
+    {
+        return isset(self::$data[$key]) ? self::$data[$key] : null;
+    }
+
+    /**
+     * Removes value from the registry by key
+     *
+     * @param string $key
+     * @return void
+     */
+    final public static function removeProduct($key)
+    {
+        if (isset(self::$data[$key])) {
+            unset(self::$data[$key]);
+        }
+    }
+}
+
+/*
+ * =====================================
+ *           USING OF REGISTRY
+ * =====================================
+ */
+
+Product::set('name', 'First product');
+
+print_r(Product::get('name'));
+// First product
