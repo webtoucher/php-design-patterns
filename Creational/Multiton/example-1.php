7438dfc2d5f138bbd9fb24883c4e90d47f74ace2
Алексей Кузнецов <kuznetsov.aleksey@caecber.ru>
2014-03-08 01:54:40 +0700

    Fixed Multiton example

--- Creational/Multiton/example-1.php
+++ Creational/Multiton/example-1.php
@@ -108,63 +108,6 @@ abstract class Factory extends FactoryAbstract
     }
 }
 
-/**
- * Multiton abstract class
- *
- * @package Patterns
- */
-abstract class RegistryFactory extends FactoryAbstract
-{
-
-    /**
-     * Returns singleton by ID
-     *
-     * @param integer|string $id - singleton's ID
-     * @return static
-     */
-    final public static function getInstance($id)
-    {
-        $className = static::getClassName();
-        if (isset(self::$instances[$className])) {
-            if (!isset(self::$instances[$className][$id])) {
-                self::$instances[$className][$id] = new $className($id);
-            }
-        } else {
-            self::$instances[$className] = [
-                $id => new $className($id),
-            ];
-        }
-        return self::$instances[$className][$id];
-    }
-
-    /**
-     * Removes singleton by ID
-     *
-     * @param integer|string $id - singleton's ID. All class instances will be removed if ID is not set
-     * @return void
-     */
-    final public static function removeInstance($id = null)
-    {
-        $className = static::getClassName();
-        if (isset(self::$instances[$className])) {
-            if (is_null($id)) {
-                unset(self::$instances[$className]);
-            } else {
-                if (isset(self::$instances[$className][$id])) {
-                    unset(self::$instances[$className][$id]);
-                }
-                if (empty(self::$instances[$className])) {
-                    unset(self::$instances[$className]);
-                }
-            }
-        }
-    }
-
-    protected function __construct($id)
-    {
-    }
-}
-
 /*
  * =====================================
  *           USING OF MULTITON
